c13c37a59df1b5ec19f759dca8f32de211edc7e1
Alires Rosa <alires.cardoso@ufpr.br>
2025-01-23 18:17:22 -0300

    feat: adicionar estrutura e estilo

---
+++ article-preview-component-master/script.js
@@ -0,0 +1,7 @@
+// document.addEventListener('mouseover', function(event) {
+//     console.log('Mouse over event detected:', event);
+// });
+
+function mostrarRedes() {
+    alert("Este é um alerta!");
+  }
