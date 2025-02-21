e40f8f4500a2c008e7a86c6db111647540bfbff1
Alires Rosa <alires.cardoso@ufpr.br>
2025-02-20 22:28:44 -0300

    feat: configurar projeto Vue com Vite

---
+++ faq-accordion-main/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="app">
+      <h1>Bem-vindo ao Vue com Vite!</h1>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+ 
+  </script>
+  
+  <style scoped>
+  h1 {
+    color: #42b983;
+  }
+  </style>
+  
