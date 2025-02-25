909c90ee5c6e9b827ff1114bb8c87d2fbcdbebae
Alires Rosa <alires.cardoso@ufpr.br>
2025-02-24 22:43:01 -0300

    feat: adicionar estrutura inicial do projeto com Vue e Vite

---
+++ 3-column-preview-card-component-main/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p>App funcionando</p>
+    </div>
+</template>
+
+<script setup>
+console.log('aqui')
+</script>
+
+<style scoped>
+p{
+    color : red;
+}
+</style>
