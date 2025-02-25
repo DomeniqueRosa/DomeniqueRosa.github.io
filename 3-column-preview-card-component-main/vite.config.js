1e05594f135adf865bc8770cff5c97da0cc5ff33
Alires Rosa <alires.cardoso@ufpr.br>
2025-02-25 13:04:25 -0300

    feat: remover arquivos de distribuição

--- 3-column-preview-card-component-main/vite.config.js
+++ 3-column-preview-card-component-main/vite.config.js
@@ -4,4 +4,7 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    assetsDir: 'assets',  // Coloca as imagens na pasta 'assets' dentro de 'dist'
+  },
 });
